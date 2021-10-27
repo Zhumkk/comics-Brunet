cff9f9a6d171079f3f5efdc6a55092470e29731b
Zhumkk <santiago.utn.br@gmail.com>
2021-10-27 08:05:34 -0300

    Desafio 2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <ul>
+                    <li><a href="#">Santiago</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
