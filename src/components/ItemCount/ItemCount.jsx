1137b9261acc1e0c57a8bcdcfedc867eeb7b08ce
Zhumkk <santiago.utn.br@gmail.com>
2021-11-06 22:18:38 -0300

    desafio 4

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { makeStyles, Button } from '@material-ui/core';
+import { itemCountStyle } from './ItemCountStyle';
+
+const useStyles = makeStyles((theme) => itemCountStyle(theme));
+
+export const ItemCount = ({stock, initial, etiquetaBoton}) => {
+    const classes = useStyles();
+    const [count, setCount] = useState(initial);
+
+    
+    return  <div className={classes.container}>
+                <h5>Cantidad disponilbe: {stock-count} unidades</h5>
+                <div>
+                    <Button onClick={e => {setCount(count-1)}} disabled={count===0}>-</Button>
+                    <h4>{count}</h4>
+                    <Button onClick={e => {setCount(count+1)}} disabled={count===10}>+</Button>
+                </div>
+                <Button>{etiquetaBoton}</Button>
+            </div>
+}
